c4b9938cc43ec4496a1978e0171bd896e0726e31
Pozymandiaz <lucasverstraete01@gmail.com>
2024-04-17 09:38:42 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let publicUrls = [
+    '/url1',
+    '/url2',
+    '/login'
+]
+
+function myMiddleware(req, res, next) {
+    const requestedUrl = req.url;
+
+    // Vérifier si l'URL demandée est publique
+    if (publicUrls.includes(requestedUrl)) {
+        next(); // Passer au middleware suivant
+    } else {
+        // Vérifier si l'utilisateur est authentifié
+        const token = req.headers['token'];
+        if (token && token === '42') {
+            next(); // Passer au middleware suivant
+        } else {
+            // Renvoyer une erreur 403 pour accès interdit
+            res.status(403).send();
+        }
+    }
+}
+
+app.use(myMiddleware)
+
+app.post('/login', (req, res) => {
+    // Générer un token unique et renvoyer par cookie
+    res.json({ token: '42' });
+})
+
+app.get('/url1', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/url2', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/private/url1', (req, res) => {
+    res.send('Hello it is secret')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
